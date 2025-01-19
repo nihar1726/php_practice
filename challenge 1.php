c1b03cc8442f763a0cd93def69cd780fe814cd1e
Nihar <158161940+nihar1726@users.noreply.github.com>
2025-01-19 09:42:18 +0530

    .

--- challenge 1.php
+++ challenge 1.php
@@ -6,8 +6,8 @@ $friend = 'Satyam';
 $candy = 'Twix';
 
 echo "<p>Hello, my name is <h1>$name</h1>.</p>";
-echo "<p>I live in $city and the cinema hall we're going is in CP67 which is a really cool mall with live music and vritual reality and a cool japanese restaurant.</p>";
+echo "<p>I live in $city and the cinema hall we're going is in CP67 which is a really cool mall with live music and virtual reality and a cool japanese restaurant.</p>";
 
-echo "<p>My movie we're going to see is <i>$movie</i>.</p>";
+echo "<p>The movie we're going to see is <i>$movie</i>.</p>";
 echo "<p>My best friend is <b>$friend</b>.</p>";
 echo "<p>My favorite candy is $candy.</p>";
