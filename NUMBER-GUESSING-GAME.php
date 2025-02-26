0085a0acd006b2c7dbba19d2a9e4c48e8d4bd725
Nihar <158161940+nihar1726@users.noreply.github.com>
2025-02-26 21:26:23 +0530

    Joe sir's solution

--- NUMBER-GUESSING-GAME.php
+++ NUMBER-GUESSING-GAME.php
@@ -2,17 +2,38 @@
 
 $min = 1;
 $max = 50;
-$num = rand($min, $max);
-$guess = 16;
+$num = /*rand($min, $max);*/ 48;
+$guess = 48;
 
-if ($guess < $min || $guess > $max) {
-    echo "Your guess is out of range.";
-} elseif ($guess < $num) {
-    echo "Your guess is too low.";
-} elseif ($guess > $num) {
-    echo "Your guess is too high.";
-} else {
-    echo "Congratulations! You guessed the number!";
+// if ($guess < $min || $guess > $max) {
+//     echo "Your guess is out of range.";
+// } elseif ($guess < $num) {
+//     echo "Your guess is too low.";
+// } elseif ($guess > $num) {
+//     echo "Your guess is too high.";
+// } else {
+//     echo "Congratulations! You guessed the number!";
+// }
+
+if ($guess < $min || $guess > $max)
+{
+    echo "<p>Your guess is out of range.</p>";
 }
-
+else {
+    if($guess == $num)
+    {
+        echo "<p>Congratulations! You guessed the number!</p>";
+    }
+    else
+    {
+        if($guess < $num)
+        {
+            echo "<p>Your guess is too low.</p>";
+        }
+        else
+        {
+            echo "<p>Your guess is too high.</p>";
+        }
+    }
+}
 
