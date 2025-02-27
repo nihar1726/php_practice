5c6650faed59766b0952e33e72b20fdad7a9321e
Nihar <158161940+nihar1726@users.noreply.github.com>
2025-02-27 19:22:41 +0530

    Constructors

---
+++ Experiment 3 (B).php
@@ -0,0 +1,32 @@
+<?php
+
+//Write a program to understand the concept of default and parameterized constructor in PHP.
+
+class Student
+{
+    public $name;
+    public $rollno;
+    public $marks;
+
+    function __construct($name = "Um... Ya forgot your name?!", $rollno = 0, $marks = 0) //combined constructor
+    {
+        $this->name = $name;
+        $this->rollno = $rollno;
+        $this->marks = $marks;
+    }
+
+    function display()
+    {
+        echo "Name: " . $this->name . "<br>";
+        echo "Roll No: " . $this->rollno . "<br>";
+        echo "Marks: " . $this->marks . "<br>";
+    }
+}
+
+echo 'This is happening becuase of the default constructor<br>';
+$student1 = new Student();
+$student1->display();
+
+echo '<br>This is happening becuase of the parameterized constructor<br>';
+$student2 = new Student("Visenya Targaryen", 101, 90);
+$student2->display();
